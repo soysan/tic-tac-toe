d63d6324725ffc1c36adfdcb15adbc9eee011e21
Masayuki <soy2.san@gmail.com>
2021-09-09 14:21:44 +0900

    ⚡️ add logic of game

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 const target = document.getElementById('target');
 class Model {
-  state = [["","",""],["","",""],["","",""]]
+  static state = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  static horizontalCheck = () => {
+    for (let i = 0; i < this.state.length; i++){
+      const currVal = this.state[i][0];
+      for (let j = 1; j < this.state[i].length; j++){
+        if (this.state[i][j] == null || currVal !== this.state[i][j]) break;
+        if (j == 2) return true;
+      }
+    }
+    return false;
+  }
+
+  static verticalCheck = () => {
+    for (let i = 0; i < this.state.length; i++){
+      const currVal = this.state[0][i];
+      for (let j = 1; j < this.state.length; j++){
+        if (this.state[j][i] == null || currVal !== this.state[j][i]) break;
+        if (j == 2) return true;
+      }
+    }
+    return false;
+  }
+
+  static diagonalCheck = () => {
+    const rightUp = [this.state[2][0], this.state[1][1], this.state[0][2]];
+    const rightDown = [this.state[0][0], this.state[1][1], this.state[2][2]];
+    const upCheck = rightUp.every(val => val != null && val == rightUp[0]);
+    const downCheck = rightDown.every(val => val != null && val == rightDown[0]);
+    return upCheck || downCheck;
+  }
 }
 class View {
   initialView = () => {
@@ -20,37 +54,45 @@ class View {
 
   squareMaker = () => {
     let squares = '<div class="square-container">'
-    for (let i = 0; i < 9; i++) {
-      const square =
-        `
-        <div id="sec${i}" class="square"></div>
-        `;
-      squares += square;
+    for (let i = 0; i < Model.state.length; i++) {
+      for (let j = 0; j < Model.state[i].length; j++) {
+        const square =
+          `
+          <div id="sec${i}${j}" class="square"></div>
+          `;
+        squares += square;
+      }
     }
     squares += '</div>'
     return squares;
   }
 }
 class Controller {
-  static addMark = () => {
-    for (let i = 0; i < 9; i++) {
-      const curr = document.querySelectorAll(`#sec${i}`)[0];
-      curr.addEventListener('click', () => {
-        if (curr.childNodes.length === 1) return;
+  static addMarkAndEvaluateGame = () => {
+    for (let i = 0; i < Model.state.length; i++) {
+      for (let j = 0; j < Model.state[i].length; j++){
+        const curr = document.querySelectorAll(`#sec${i}${j}`)[0];
+        curr.addEventListener('click', () => {
+          if (curr.childNodes.length === 1) return;
 
-        const playerNum = document.querySelectorAll('#player')[0];
-        const h1 = document.createElement('h1');
-        h1.classList.add('mark');
+          const playerNum = document.querySelectorAll('#player')[0];
+          const h1 = document.createElement('h1');
+          h1.classList.add('mark');
 
-        if (playerNum.innerText === '1') {
-          h1.innerText = "◯";
+          if (playerNum.innerText === '1') {
+            h1.innerText = "◯";
+          } else {
+            h1.innerText = "×";
+          }
+          Model.state[i][j] = h1.innerText;
           curr.append(h1);
-        } else {
-          h1.innerText = "×";
-          curr.append(h1);
-        }
-        this.changePlayerNum(playerNum, h1.innerText);
-      })
+
+          if (this.evaluateWin()) alert(`Player ${playerNum.innerText} is win!`);
+          else if (this.checkAllFill()) alert('Draw Game');
+
+          this.changePlayerNum(playerNum, h1.innerText);
+        })
+      }
     }
   }
 
@@ -59,14 +101,27 @@ class Controller {
   }
 
   static evaluateWin = () => {
-
+    const horizontal = Model.horizontalCheck();
+    const vertical = Model.verticalCheck();
+    const diagonal = Model.diagonalCheck();
+    return horizontal || vertical || diagonal;
   }
 
   static checkAllFill = () => {
-    
+    const state = Model.state;
+    for (let i of state) {
+      for (let j of i) {
+        if (j === null) return false;
+      }
+    }
+    return true;
+  }
+
+  static startNewGame = () => {
+    const start = new View()
+    start.initialView()
+    Controller.addMarkAndEvaluateGame();
   }
 }
 
-const start = new View()
-start.initialView()
-Controller.addMark();
+Controller.startNewGame();
